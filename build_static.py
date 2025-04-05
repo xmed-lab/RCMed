35a69c664a107df37683cca8635e289ba35f1132
Haonan Wang <haonan1wang@gmail.com>
2025-04-05 21:28:26 +0800

    Fix GitHub Pages deployment and add 404 page

--- build_static.py
+++ build_static.py
@@ -19,15 +19,39 @@ def build_static_site():
     # Get cases data
     cases = get_cases_from_files()
     
+    # Create a simple url_for replacement
+    def url_for(endpoint, filename=None):
+        if endpoint == 'static':
+            return f'static/{filename}'
+        return ''
+    
     # Render template
     html_content = template.render(
         cases=cases,
-        url_for=lambda endpoint, filename: f'/RCMed/static/{filename}'
+        url_for=url_for
     )
     
     # Write index.html
     with open('build/index.html', 'w', encoding='utf-8') as f:
         f.write(html_content)
+    
+    # Create a simple index.html in the root that redirects to build/index.html
+    with open('build/404.html', 'w', encoding='utf-8') as f:
+        f.write('''
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>RCMed - Medical Image Analysis</title>
+    <script>
+        window.location.href = "/RCMed/";
+    </script>
+</head>
+<body>
+    <p>Redirecting to <a href="/RCMed/">RCMed</a>...</p>
+</body>
+</html>
+        ''')
 
 if __name__ == '__main__':
     build_static_site()
